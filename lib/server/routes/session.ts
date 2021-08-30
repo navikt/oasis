25afc5d9392bc5f71686dceaf1de9ab1ff2d1079
André Roaldseth <andre.roaldseth@nav.no>
2021-08-30 12:34:23 +0200

    Ta med expires_in i session-responsen
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- lib/server/routes/session.ts
+++ lib/server/routes/session.ts
@@ -2,14 +2,15 @@ import { NextApiHandler, NextApiRequest } from "next";
 import { getSession } from "../../client";
 
 const session: NextApiHandler = async (req: NextApiRequest, res) => {
-  const { token } = await getSession({ req });
+  const { token, payload } = await getSession({ req });
 
   if (!token) {
     res.status(401).end();
     return;
   }
 
-  res.json({});
+  const expiresIn = payload.exp - Date.now();
+  res.json({ expires_in: expiresIn });
 };
 
 export default session;
