6d31cf87f89ad5df7d7b32f575200fd1dc9d2af5
André Roaldseth <andre.roaldseth@nav.no>
2022-10-26 14:32:00 +0200

    Forbedre exports enda et knepp

---
+++ lib/identity-providers/index.ts
@@ -0,0 +1,2 @@
+export { default as azure } from "./azure";
+export { default as idporten } from "./idporten";
---
+++ lib/obo-providers/index.ts
@@ -0,0 +1,3 @@
+export { default as azure } from "./azure";
+export { default as tokenX } from "./tokenx";
+export { withInMemoryCache } from "./withInMemoryCache";
