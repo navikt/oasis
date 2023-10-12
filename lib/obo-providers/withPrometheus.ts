b7a7e09a99e4cab9c5f41d3794eb5120ddfead17
André Roaldseth <andre.roaldseth@nav.no>
2023-10-12 14:20:17 +0200

    Legg på debug med console.log

--- lib/obo-providers/withPrometheus.ts
+++ lib/obo-providers/withPrometheus.ts
@@ -1,9 +1,11 @@
 import { OboProvider } from "../index";
 import prometheus from "../metrics/prometheus";
+import { register } from "prom-client";
 
 export function withPrometheus(oboProvider: OboProvider): OboProvider {
   const provider = oboProvider.name;
   return async (token, audience) => {
+    console.log("Doing exchcange");
     const measureTokenExchange = prometheus.tokenExchangeDurationHistogram
       .labels({ provider })
       .startTimer();
@@ -16,6 +18,11 @@ export function withPrometheus(oboProvider: OboProvider): OboProvider {
       prometheus.tokenExchangeFailures.labels({ provider }).inc();
       return oboToken;
     }
+
+    console.log("Successfull exchange. Metrics should have values now");
+    console.log(
+      await register.getSingleMetric("dp_auth_token_exchanges")!.get()
+    );
 
     return oboToken;
   };
