5396f043393a8202f7c7866124c7443ed4429094
André Roaldseth <andre.roaldseth@nav.no>
2023-10-12 13:02:32 +0200

    Legg på withPrometheus() for tokenX i test-appen

--- lib/provider.ts
+++ lib/provider.ts
@@ -3,12 +3,13 @@ import idporten from "./identity-providers/idporten";
 import azure from "./identity-providers/azure";
 import azureOBO from "./obo-providers/azure";
 import tokenX from "./obo-providers/tokenx";
+import { withPrometheus } from "./obo-providers/withPrometheus";
 
 export let getSession: GetSessionWithOboProvider;
 if (process.env.PROVIDER == "idporten") {
   getSession = makeSession({
     identityProvider: idporten,
-    oboProvider: tokenX,
+    oboProvider: withPrometheus(tokenX),
   });
 } else if (process.env.PROVIDER == "azure") {
   getSession = makeSession({
