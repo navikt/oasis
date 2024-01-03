190e50a12bfdb3528e6a28668f69519c5223a18f
André Roaldseth <andre.roaldseth@nav.no>
2024-01-03 14:34:00 +0100

    Bruk InMemoryCache for test-appene

--- lib/provider.ts
+++ lib/provider.ts
@@ -4,16 +4,17 @@ import azure from "./identity-providers/azure";
 import azureOBO from "./obo-providers/azure";
 import tokenX from "./obo-providers/tokenx";
 import { withPrometheus } from "./obo-providers/withPrometheus";
+import { withInMemoryCache } from "./obo-providers";
 
 export let getSession: GetSessionWithOboProvider;
 if (process.env.PROVIDER == "idporten") {
   getSession = makeSession({
     identityProvider: idporten,
-    oboProvider: withPrometheus(tokenX),
+    oboProvider: withInMemoryCache(withPrometheus(tokenX)),
   });
 } else if (process.env.PROVIDER == "azure") {
   getSession = makeSession({
     identityProvider: azure,
-    oboProvider: withPrometheus(azureOBO),
+    oboProvider: withInMemoryCache(withPrometheus(azureOBO)),
   });
 }
