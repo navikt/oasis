8323b03c40e56d59871bd1e0d207306e726ef910
Andreas Nordahl <andnorda@gmail.com>
2024-02-05 15:11:35 +0100

    remove unused(?) code

--- example-app/pages/_app.tsx
+++ example-app/pages/_app.tsx
@@ -1,9 +1,5 @@
 import { AppProps } from "next/app";
 
-if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
-  //require("../../oasis/src/mocks");
-}
-
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
