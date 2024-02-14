1894d45ae4179cc14c0471f3865308dc59b52313
Andreas Nordahl <andnorda@gmail.com>
2024-02-14 13:00:52 +0100

    remove idporten acr validation (#109)
    * remove idporten acr validation
    
    * remove unused import

--- oasis/src/identity-providers/idporten.ts
+++ oasis/src/identity-providers/idporten.ts
@@ -1,4 +1,4 @@
-import { JWTVerifyResult, errors, jwtVerify } from "jose";
+import { JWTVerifyResult, jwtVerify } from "jose";
 import { SupportedRequestType, Token } from "../index";
 import { cachedRemoteJWKSet } from "../utils/cachedRemoteJWKSet";
 import { getTokenFromHeader } from "../utils/getTokenFromHeader";
@@ -7,23 +7,10 @@ const idportenJWKSet = () =>
   cachedRemoteJWKSet(process.env.IDPORTEN_JWKS_URI as string);
 
 async function verify(token: string): Promise<JWTVerifyResult> {
-  const result = await jwtVerify(token, idportenJWKSet(), {
+  return jwtVerify(token, idportenJWKSet(), {
     issuer: process.env.IDPORTEN_ISSUER,
     audience: process.env.IDPORTEN_AUDIENCE,
   });
-
-  const acr = process.env.IDPORTEN_REQUIRED_ACR
-    ? [process.env.IDPORTEN_REQUIRED_ACR]
-    : ["Level4", "idporten-loa-high"];
-  // @ts-ignore
-  if (result.payload["acr"] in acr) {
-    throw new errors.JWTClaimValidationFailed(
-      `unexpected "acr" claim value, expected "${acr}", recieved"${result.payload["acr"]}"`,
-      "acr",
-      "check_failed"
-    );
-  }
-  return result;
 }
 
 export default async function idporten(
