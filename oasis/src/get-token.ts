dbab042705fcff55e6812bd93854379018f5af66
Andreas Nordahl <andnorda@gmail.com>
2024-02-21 12:25:40 +0100

    add support for node Request to getToken (#124)
    * add support for node Request to getToken
    
    * oops

--- oasis/src/get-token.ts
+++ oasis/src/get-token.ts
@@ -1,11 +1,18 @@
 import { IncomingMessage } from "http";
 
-export const getToken = (val: IncomingMessage | string): string | undefined => {
+export const getToken = (
+  val: Request | IncomingMessage | string,
+): string | undefined => {
   if (typeof val === "string") {
     return val.startsWith("Bearer ") ? val.replace("Bearer ", "") : val;
   } else {
-    return val.headers.authorization
-      ? getToken(val.headers.authorization)
-      : undefined;
+    const { headers } = val;
+    let authHeader;
+    if ("authorization" in headers) {
+      authHeader = headers.authorization;
+    } else if (headers instanceof Headers) {
+      authHeader = headers.get("authorization");
+    }
+    return authHeader ? getToken(authHeader) : undefined;
   }
 };
