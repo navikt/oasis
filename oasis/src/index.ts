90fb05a466dd4c23d9eb476929ba05ab76ad3729
Karl J. Overå <k@rl.run>
2024-08-12 12:20:03 +0200

    Bedre deprekering av OboResult

--- oasis/src/index.ts
+++ oasis/src/index.ts
@@ -3,12 +3,25 @@ export {
   requestAzureOboToken,
   requestTokenxOboToken,
 } from "./obo";
-export {
-  requestAzureClientCredentialsToken,
-} from "./client-credentials"
+export { requestAzureClientCredentialsToken } from "./client-credentials";
+
 // To keep the API stable after OboResult was renamed to TokenResult
 // can be removed in a potential major version bump in the future
-export { TokenResult as OboResult } from "./token-result";
+// had to mess around with imports to get the deprecation to show in editors
+import { TokenResult } from "./token-result";
+/**
+ * @deprecated Use {@link TokenResult} instead
+ */
+const OboResult = TokenResult;
+
+/**
+ * @deprecated Use {@link TokenResult} instead
+ */
+type OboResult = TokenResult;
+
+export { OboResult };
+// Deprecated section end
+
 export {
   validateToken,
   validateAzureToken,
