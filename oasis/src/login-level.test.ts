57da6c32b91ebc3ff6673bec3873b097773d4215
Ole Peder Lømo <101403961+olelomo-nav@users.noreply.github.com>
2025-02-13 12:41:40 +0100

    Funksjon for å sjekke forventet nivå (#385)
    * Add acr to IdPortenPayload response
    
    Co-authored-by: Bjørn Gilstad <bjorn.gilstad@nav.no>
    
    * Add function isLoginLevelHigh
    
    * Remove acr from idporten payload
    
    * Rewrite to boolean that takes in a pre defined login level
    
    * Add function documentation
    
    ---------
    
    Co-authored-by: Bjørn Gilstad <bjorn.gilstad@nav.no>

---
+++ oasis/src/login-level.test.ts
@@ -0,0 +1,46 @@
+import { isIdportenLoginLevel } from "./login-level";
+import { token } from "./test-provider";
+
+describe("login level", () => {
+  it("should return true for login level High when acr is idporten-loa-high", async () => {
+    const res = isIdportenLoginLevel(
+      "High",
+      await token({ acr: "idporten-loa-high" }),
+    );
+    expect(res).toBe(true);
+  });
+
+  it("should return false for login level High when acr is idporten-loa-substantial", async () => {
+    const res = isIdportenLoginLevel(
+      "High",
+      await token({ acr: "idporten-loa-substantial" }),
+    );
+    expect(res).toBe(false);
+  });
+
+  it("should return true for login level Substantial when acr is idporten-loa-high", async () => {
+    const res = isIdportenLoginLevel(
+      "Substantial",
+      await token({ acr: "idporten-loa-substantial" }),
+    );
+    expect(res).toBe(true);
+  });
+
+  it("should return false for login level Substantial when acr is idporten-loa-substantial", async () => {
+    const res = isIdportenLoginLevel(
+      "Substantial",
+      await token({ acr: "idporten-loa-high" }),
+    );
+    expect(res).toBe(false);
+  });
+
+  it("throws error for invalid token", async () => {
+    expect(() => isIdportenLoginLevel("High", "")).toThrow();
+  });
+
+  it("throws error for token without acr", async () => {
+    await expect(async () =>
+      isIdportenLoginLevel("High", await token()),
+    ).rejects.toThrow();
+  });
+});
---
+++ oasis/src/login-level.ts
@@ -0,0 +1,22 @@
+import { decodeJwt } from "jose";
+
+type Level = "High" | "Substantial";
+
+/**
+ * Returns true if the token has the login level specified.
+ *
+ * @param level IdPorten login level to check for.
+ * @param token IdPorten token level with acr payload.
+ */
+export const isIdportenLoginLevel = (level: Level, token: string): boolean => {
+  const { acr } = decodeJwt(token);
+
+  if (typeof acr === "string") {
+    if (level === "High") {
+      return acr === "idporten-loa-high";
+    } else if (level === "Substantial") {
+      return acr === "idporten-loa-substantial";
+    }
+  }
+  throw Error("acr is either missing or invalid");
+};
