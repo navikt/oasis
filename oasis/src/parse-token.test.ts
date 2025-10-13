91ed3e0f876b7d17cfdb22c98602f2ff82ee7267
Karl J. Overå <k@rl.run>
2025-10-13 18:10:35 +0200

    chore: put test utils in their own folder

--- oasis/src/parse-token.test.ts
+++ oasis/src/parse-token.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from "vitest";
 
 import { parseAzureUserToken, parseIdportenToken } from "./parse-token";
-import { expectNotOK, expectOK } from "./test-expect";
-import { token } from "./test-provider";
+import { expectNotOK, expectOK } from "./test-utils/test-expect";
+import { token } from "./test-utils/test-provider";
 
 describe("parseIdportenToken", () => {
   it("should return pid", async () => {
