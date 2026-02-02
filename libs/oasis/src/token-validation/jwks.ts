5d4d4b794d952fb27653869b4887ac91055fd949
sigurdgroneng <sigurd.groneng@nav.no>
2026-02-02 13:42:13 +0100

    Increase remote jwks-set max age to 24 hours

--- libs/oasis/src/token-validation/jwks.ts
+++ libs/oasis/src/token-validation/jwks.ts
@@ -1,5 +1,7 @@
 import { createRemoteJWKSet } from "jose";
 
+const ONE_DAY_IN_MILLIS = 1000 * 60 * 60 * 24;
+
 const remoteJWKSetCache: Record<
   string,
   ReturnType<typeof createRemoteJWKSet>
@@ -8,7 +10,9 @@ export function getJwkSet(
   jwksUri: string,
 ): ReturnType<typeof createRemoteJWKSet> {
   if (remoteJWKSetCache[jwksUri] == null) {
-    remoteJWKSetCache[jwksUri] = createRemoteJWKSet(new URL(jwksUri));
+    remoteJWKSetCache[jwksUri] = createRemoteJWKSet(new URL(jwksUri), {
+      cacheMaxAge: ONE_DAY_IN_MILLIS,
+    });
   }
 
   return remoteJWKSetCache[jwksUri];
