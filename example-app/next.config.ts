d810689b14b4a56a399cc0f8d8ac4f5d5df64b9b
Karl J. Overå <k@rl.run>
2026-09-04 10:21:38 +0200

    chore: kill the example apps

--- example-app/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  output: "standalone",
-};
-
-export default nextConfig;
--- example-app/pages/api/internal/isalive.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function isAliveHandler(
-  _req: NextApiRequest,
-  res: NextApiResponse<string>,
-) {
-  res.status(200).send("Alive");
-}
